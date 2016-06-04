b1620d0467cbda4590c4f5f2c8f5999c6e5f8adb
Diogo <diogo1243@gmail.com>
2016-06-04 17:13:54 -0300

    Almost perfect pool version, will do it in a branch and merge it with the master branch.

--- Assets/Scripts/Enemy/EnemySpawner.cs
+++ Assets/Scripts/Enemy/EnemySpawner.cs
@@ -5,19 +5,10 @@ using System.Collections.Generic;
 
 public class EnemySpawner : NetworkBehaviour
 {
-    [SerializeField] float pooledEnemiesAmount = 20;
     [SerializeField] float enemySpawnDelay = 1f;
-    [SerializeField] float enemySpeed = 6;
 
     private float elapsedTime;
     private bool gameStarted = false;
-
-    List<GameObject> enemyPool;
-
-    public override void OnStartServer()
-    {
-        gameStarted = true;
-    }
 
     void Awake()
     {
@@ -26,67 +17,25 @@ public class EnemySpawner : NetworkBehaviour
 
     void Start ()
     {
-        enemyPool = new List<GameObject>();
-        for (int i = 0; i < pooledEnemiesAmount; i++)
-            CmdPoolEnemy();
-
         elapsedTime = enemySpawnDelay;
     }
 
-    [Command]
-    void CmdPoolEnemy()
-    {
-        GameObject enemy = (GameObject) Instantiate(Resources.Load("Enemy1", typeof(GameObject)), transform.position, transform.rotation);
-        enemy.SetActive(false);
-		enemy.transform.SetParent (transform);
-        enemy.GetComponent<Rigidbody>().velocity = transform.forward * enemySpeed;
-        NetworkServer.Spawn(enemy);
-        enemyPool.Add(enemy);
-        //Destroy(enemy, 10);
-    }
+	public override void OnStartServer()
+	{
+		gameStarted = true;
+	}
 
-    [Command]
-    void CmdActivateEnemy()
-    {
-        for (int i = 0; i < pooledEnemiesAmount; i++)
-        {
-            if (!enemyPool[i].activeInHierarchy)
-            {
-                enemyPool[i].transform.position = transform.position;
-                enemyPool[i].transform.rotation = transform.rotation;
-                enemyPool[i].SetActive(true);
-                RpcActivateEnemy();
-                return;
-            }
-        }
-    }
-
-    [ClientRpc]
-    void RpcActivateEnemy()
-    {
-        if (!isServer)
-            for (int i = 0; i < pooledEnemiesAmount; i++)
-            {
-                if (!enemyPool[i].activeInHierarchy)
-                {
-                    enemyPool[i].transform.position = transform.position;
-                    enemyPool[i].transform.rotation = transform.rotation;
-                    enemyPool[i].SetActive(true);
-                    return;
-                }
-            }
-    }
-
-    void FixedUpdate ()
-    {
-        if (gameStarted)
-        {
-            elapsedTime += Time.deltaTime;
-            if (elapsedTime >= enemySpawnDelay)
-            {
-                CmdActivateEnemy();
-                elapsedTime = 0;
-            }
-        }
-    }
+	void FixedUpdate()
+	{
+		if (gameStarted && isServer)
+		{
+			//RpcSyncClientServerPooledObjects();
+			elapsedTime += Time.deltaTime;
+			if (elapsedTime >= enemySpawnDelay)
+			{
+				GetComponent<PoolingObjectHandler> ().ServerCreateFromPool(transform.position, transform.rotation);
+				elapsedTime = 0;
+			}
+		}
+	}
 }
