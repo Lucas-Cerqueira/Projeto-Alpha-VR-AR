a6e43c2daefc9e6c36efaf0fca7514cf2ad77c12
Diogo <diogo1243@gmail.com>
2016-06-13 21:57:49 -0300

    Changed some things in the shooter UI.

--- Assets/Scripts/UI/HealthHandler.cs
+++ Assets/Scripts/UI/HealthHandler.cs
@@ -5,18 +5,20 @@ using UnityEngine.Networking;
 
 public class HealthHandler : NetworkBehaviour {
 
-
-	Text healthText;
+	[SerializeField] int numOfBars = 10;
+	GameObject healthText;
 	private Combat myShooter;
-
+	private int prevHealth = 0;
+	private int prevChanged = 10;
 
 	// Use this for initialization
 	void Start () 
     {
 		if (isLocalPlayer) 
 		{
-			healthText = GameObject.Find ("LifePercentage").GetComponent<Text> ();
+			healthText = GameObject.Find ("LifePercentage");
 			myShooter = GetComponent<Combat> ();
+			prevHealth = myShooter.health;
 		}
 	}
 	
@@ -25,10 +27,24 @@ public class HealthHandler : NetworkBehaviour {
     {
         if (isLocalPlayer)
         {
-            if (healthText)
-                healthText.text = myShooter.health + "%";
+			if (healthText) 
+			{
+				if (myShooter.health != prevHealth) 
+				{
+					prevHealth = myShooter.health;
+					OnHealthChange ();
+				}
+			}
             else
-                if (healthText = GameObject.Find("LifePercentage").GetComponent<Text>()) { }
+                if (healthText = GameObject.Find("LifePercentage")) { }
         }
+	}
+
+	public void OnHealthChange ()
+	{
+		int activeHealth = prevHealth / numOfBars;
+		for (int i = (activeHealth > prevChanged ? prevChanged : activeHealth); i< (activeHealth > prevChanged ?  activeHealth : prevChanged); i++)
+			healthText.transform.GetChild (i).gameObject.SetActive (!healthText.transform.GetChild (i).gameObject.activeSelf);
+		prevChanged = activeHealth;
 	}
 }
