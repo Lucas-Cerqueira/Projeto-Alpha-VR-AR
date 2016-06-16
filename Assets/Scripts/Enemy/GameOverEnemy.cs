7b0ebe57d232cebf106806e54348bd918d13f41a
Diogo <diogo1243@gmail.com>
2016-06-15 22:47:16 -0300

    Fixed some bugs on NavMesh

--- Assets/Scripts/Enemy/GameOverEnemy.cs
+++ Assets/Scripts/Enemy/GameOverEnemy.cs
@@ -4,61 +4,62 @@ using UnityEngine.Networking;
 
 public class GameOverEnemy : GameOver{
 
-    private Animator myAnimator;
+	private Animator myAnimator;
 	private GameObject money;
 
-    public bool isDead = false;
+	public bool isDead = false;
 
 	// Use this for initialization
-		
-    void Start()
-    {
-        money = GameObject.Find("Money");
-        myAnimator = GetComponent<Animator>();
-    }
-	
-    void OnEnable()
-    {
-        transform.GetChild(1).GetComponent<CapsuleCollider>().enabled = true;
+
+	void Start()
+	{
+		money = GameObject.Find("Money");
+		myAnimator = GetComponent<Animator>();
+	}
+
+	void OnEnable()
+	{
+		transform.GetChild(1).GetComponent<CapsuleCollider>().enabled = true;
 		GetComponent<Navigation_Enemy> ().enabled = true;
-    }
+		GetComponent<NavMeshAgent> ().enabled = true;
+	}
 
 	// Update is called once per frame
 	void Update () {
-	
+
 	}
 
 	public override void EndGame ()
 	{
-        RpcDeactivateStuff();
-        GetComponent<HealthBar>().enabled = false;
-        transform.GetChild(0).gameObject.SetActive(false);
-        transform.GetChild(1).GetComponent<CapsuleCollider>().enabled = false;
-        GetComponent<NavMeshAgent>().Stop();
+		RpcDeactivateStuff();
+		GetComponent<HealthBar>().enabled = false;
+		transform.GetChild(0).gameObject.SetActive(false);
+		transform.GetChild(1).GetComponent<CapsuleCollider>().enabled = false;
+		GetComponent<NavMeshAgent>().Stop();
+		GetComponent<NavMeshAgent> ().enabled = false;
 
-        
-        //NetworkServer.Destroy(this.gameObject);
-        int i = Random.Range(1, 3);
-        myAnimator.Rebind();
-        myAnimator.SetBool("isDead" + i.ToString(), true);
+		//NetworkServer.Destroy(this.gameObject);
+		int i = Random.Range(1, 3);
+		myAnimator.Rebind();
+		myAnimator.SetBool("isDead" + i.ToString(), true);
 		if (isServer) 
 		{
 			if (money && GetComponent<Combat>().health >= 0) 
-                money.GetComponent<MoneyHandler>().AddMoney(100);
+				money.GetComponent<MoneyHandler>().AddMoney(100);
 
-            //transform.parent.GetComponent<PoolingObjectHandler>().UnSpawnObject(this.gameObject);
-            //NetworkServer.UnSpawn(this.gameObject);
+			//transform.parent.GetComponent<PoolingObjectHandler>().UnSpawnObject(this.gameObject);
+			//NetworkServer.UnSpawn(this.gameObject);
 			transform.parent.GetComponent<PoolingObjectHandler> ().ServerReturnToPool (this.gameObject, myAnimator.GetCurrentAnimatorClipInfo (0) [0].clip.length);
 		}
 	}
 
-    [ClientRpc]
-    void RpcDeactivateStuff()
-    {
-        GetComponent<HealthBar>().enabled = false;
+	[ClientRpc]
+	void RpcDeactivateStuff()
+	{
+		GetComponent<HealthBar>().enabled = false;
 		GetComponent<Navigation_Enemy> ().enabled = false;
-        transform.GetChild(0).gameObject.SetActive(false);
-        transform.GetChild(1).GetComponent<CapsuleCollider>().enabled = false;
-        GetComponent<NavMeshAgent>().Stop();
-    }
+		transform.GetChild(0).gameObject.SetActive(false);
+		transform.GetChild(1).GetComponent<CapsuleCollider>().enabled = false;
+		GetComponent<NavMeshAgent>().Stop();
+	}
 }
